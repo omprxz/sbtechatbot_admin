4ff799bc659e77f2ea9641ee1d846c8469b49faa
Om <omprxz@gmail.com>
2024-11-01 13:14:26 +0530

    Added missing db module

---
+++ src/libs/db.js
@@ -0,0 +1,11 @@
+import mysql from "mysql2/promise";
+
+export const db = async () => {
+  return await mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    port: process.env.MYSQL_PORT,
+  });
+};
